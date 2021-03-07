f4c0310c4917fdb6b3a8186399b6ca825548dd0f
Christian S <Saregadon@gmail.com>
2021-03-07 01:08:37 -0600

    Took out Linked Lists inside program

--- Test.cpp
+++ Test.cpp
@@ -10,68 +10,7 @@
 #include <numeric>
 
 using namespace std;
-/* Linked List Implementation
-struct node
-{
-    node* prev;
-    node* next;
-    string k; //keyword
-    int a; //argument
-};
 
-struct linecounter
-{
-    linecounter* next;
-    int linestart; //line starts after job #
-    int lineend; //line end ends before next # or end of file
-};
-
-void addnode(node* head, string keyword, int argument)
-{
-    node* data = new node; //allocating memory for new node
-    data->k = keyword; //adding keyword to block [i] of new node
-    data->a = argument; //adding argument to block [i] of new node
-    data->next = head; //setting next node as the head
-    head = data; //setting the head to a new nullptr node
-}
-
-//probably just implement doubly linked list
-void deletenode(node* head, node* temp) //head and temp = head->next->next (put in main)
-{
-    node* prev = head;
-    if(head == temp)
-    {
-        if(head->next == nullptr)
-        {
-            cout << "Can't transfer nullptr to next node." << endl; return;
-        }
-
-        //setting keyword and argument = to the next nodes in the list;
-        head->k = head->next->k;
-        head->a = head->next->a;
-
-        //store address of next node inside of temp variable for deletion
-        temp = head->next;
-
-        //quite literally delete temp
-        delete temp;
-        return;
-    }
-
-    while(prev->next != nullptr && prev->next != temp) prev = prev->next;
-
-    if(prev->next == nullptr)
-    {
-        cout << "Can't delete nullptr previous node" << endl; return;
-    }
-
-    //provide the next variable behind the head node to delete the previous node
-    prev->next = prev->next->next;
-
-    delete temp;
-    return;
-}
-*/
 void core_request(string how_long, int jobID, vector<int> jobqueue, bool &core)
 {
     if(core == true) //true == free
@@ -194,7 +133,7 @@ int main()
         cout << veckeyword[i] + " ";
         cout << vecargument[i] + " ";
         cout << endl;
-
+        
         if(veckeyword[i] == "JOB") //takes in the job #
         {
             jobnumber.push_back(vecargument[i]);
